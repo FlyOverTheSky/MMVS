2eab61aaeb9a7e0d4ba0e8ccd637e0a7adf96d06
FlyOverTheSky <sashka.sheshenin@gmail.com>
2024-02-17 11:26:20 +0500

    Update GET, POST tests

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,5 @@
 import os
-import time
+import random
 import uuid
 
 import requests
@@ -9,19 +9,19 @@ from dotenv import load_dotenv
 
 from rest_framework.test import APITestCase, APIRequestFactory
 
-from MMVS.settings import VIDEO_PATH
-from api.models import VideoModel
 
 load_dotenv()
 
 TEST_VIDEO_URL = os.getenv("TEST_VIDEO_URL", "")
+TEST_FILES_PATH = os.getenv("TEST_FILES_PATH", "")
 INDEX_URL = os.getenv("INDEX_URL", "")
 
 
 class VideoTestCase(APITestCase):
 
     test_video_response = requests.get(url=TEST_VIDEO_URL, stream=True)
-    test_video_path = os.path.join(VIDEO_PATH, 'initial_test_video.mp4')
+    test_video_filename = f"test_video_{random.randint(1, 100000)}.mp4"
+    test_video_path = os.path.join(TEST_FILES_PATH, test_video_filename)
 
     def setUp(self):
         with open(f"{self.test_video_path}", 'wb') as file:
@@ -45,12 +45,18 @@ class VideoTestCase(APITestCase):
         get_request = requests.get(
             url=f'{INDEX_URL}/file/{test_video_id}'
         )
-        get_response_video_id = json.loads(get_request.content.decode('utf-8')).get('id')
-
+        get_repsonse_data = json.loads(get_request.content.decode('utf-8'))
+        get_response_video_id = get_repsonse_data.get('id')
+        get_response_video_filename = get_repsonse_data.get('filename')
         self.assertEqual(
             first=uuid.UUID(get_response_video_id),
             second=test_video_id,
-            msg='Полученный ид и ид при загрузке не совпадают'
+            msg='Полученный id и id при загрузке не совпадают'
+        )
+        self.assertEqual(
+            first=f"{str(get_response_video_filename)}.mp4",
+            second=self.test_video_filename,
+            msg='Полученное имя файла и заданное не совпадают',
         )
 
     def test_delete_file(self):
